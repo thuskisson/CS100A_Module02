de9c1f80495d2fab94e0dd16c622bd36d7bded17
thuskisson <thuskisson@pga.com>
2023-10-11 16:40:50 -0700

    practice area for using strings

---
+++ String_functionality.py
@@ -0,0 +1,4 @@
+text = input("Please type a string. ")
+print(f"This is your string in all upper case: {text.upper()}")
+print(f"This is your string in all lower case: {text.lower()}")
+print(f"This is your string in 'title' case: {text.title()}")
---
+++ strings_practice.py
@@ -0,0 +1,14 @@
+pirates = '91'
+bruins = '67'
+print(pirates + bruins)
+print(int(pirates) + int(bruins))
+
+pscore = 91
+bscore = 67
+print(pscore + bscore)
+print(str(pscore) + str(bscore))
+
+print(f'Whitworth beat George Fox {pirates} to {bruins}')
+
+text = input('give me a string: ')
+print('the first character in your string is ' + text [0])
