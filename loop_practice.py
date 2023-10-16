9565d9918d6ac16a9db50e26b9c17fae41766f71
thuskisson <thuskisson@pga.com>
2023-10-15 20:54:12 -0700

    added a func to calc the  factorial from 1 to n

--- loop_practice.py
+++ loop_practice.py
@@ -6,3 +6,12 @@ for x in range(1,n):
     sum = sum + x
 # output summation 1-n
 print(f'sum from 1 to {n} is {sum}')
+
+#Get another number and convert it to an int type
+a = int(input('enter another number: '))
+factorial = 1
+#count from 1 to n
+for y in range(1,a):
+    factorial = factorial * y
+#Output the factorial (1 * 2 * 3 * 4 * ... * n)
+print (f'The factorial from 1 to {a} is {factorial}')
